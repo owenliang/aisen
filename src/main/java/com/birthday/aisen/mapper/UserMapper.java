2a1d1294101e838eb0ab68a8bd93f15104b55cbb
liangdong <liangdong@smzdm.com>
2019-05-30 18:13:40 +0800

    updtae

--- src/main/java/com/birthday/aisen/mapper/UserMapper.java
+++ src/main/java/com/birthday/aisen/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package com.birthday.aisen.mapper;
 
 import com.birthday.aisen.entity.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserMapper {
     User getUserById(long uid);
     List<User> getUsersByName(String username);
